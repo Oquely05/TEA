7892aacaec36b97afef3a433cf34af09a78b705b
Carlos Oquely Rodriguez Banegas (24269) <carlos.rodriguez@est.zamorano.edu>
2022-09-29 00:36:38 -0600

    carlos

--- Labs/Lab02/pagoporhora.py
+++ Labs/Lab02/pagoporhora.py
@@ -1,5 +1,5 @@
 hora= input ("¿Cuantas horas trabajó?")
 valor= input ("¿Cuanto se le paga por hora?")
-pagohora = int(hora)+float(valor)
+pagohora = int(hora)*float(valor)
 
-print (pagohora)
+print (pagohora) 
